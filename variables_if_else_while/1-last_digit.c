0bc6bf58b6e7e84a2e7e0daa0b58536518a85e47
Storm2601 <7471@holbertonstudents.com>
2023-10-17 05:14:09 -0700

    Si le dernier chiffre de n est supérieur à 5 : la chaîne et est supérieur à 5 si le dernier chiffre de n est 0 : la chaîne et vaut 0 si le dernier chiffre de n est inférieur à 6 et non 0 : la chaîne et est inférieure à 6 et non 0

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,15 +21,15 @@ int main(void)
 
 	if (n > 5)
 	{
-		printf("Last digit of %d is %d and greater than 5\n", n, lastnum);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastnum);
 	}
 	else if (n == 0)
 	{
-		printf("Last digit of %d is %d less than 6 and not 0\n", n,lastnum);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,lastnum);
 	}
 	else if (n < 6 && lastnum != 0)
 	{
-		printf("Last digit of %d is %d 0\n", n, lastnum);
+		printf("Last digit of %d is %d is 0\n", n, lastnum);
 	}
 	return (0);
 }
