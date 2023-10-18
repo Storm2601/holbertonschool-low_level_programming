ea82b887fd2a6f3e079e0b439081e32e4382ea1a
Storm2601 <7471@holbertonstudents.com>
2023-10-18 01:23:56 -0700

    imprime tous les nombres à un chiffre de la base 10 en commençant par 0 et doit être dans la fonction principale

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
