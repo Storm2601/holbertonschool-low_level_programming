42f76739410ee3ac3d13926915bcbf1a1119800b
Storm2601 <7471@holbertonstudents.com>
2023-10-18 04:55:32 -0700

    imprime toutes les combinaisons possibles de nombres à un chiffre et doivent être imprimés dans l’ordre croissant, Les nombres doivent être séparés par une virgule suivi d’un espace

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,40 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
-* main - Entry point
-*
-* Description: 'the program's description'
+* main - all combination of single digit numbers
 *
 * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
-	putchar(',');
-	putchar(' ');
+	int digit;
 
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit + '0');
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
-
 	return (0);
 }
