1ea89f14bbca54826299d85ac5f421d4d19ffe86
Storm2601 <7471@holbertonstudents.com>
2023-10-18 00:45:38 -0700

    imprime l’alphabet en minuscules, puis en majuscules, en utilisant que la fonction putchar et doit être dans la fonction principale

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -12,10 +12,23 @@
 
 int main(void)
 {
-	for (char c = 'a'; c <= 'z'; c++) {
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
 		putchar(c);
+		c++;
 	}
+
+	c = 'A';
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
 	putchar('\n');
 
-	return 0;
+	return (0);
 }
