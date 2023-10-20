1910678d97095f3b1a727f4a3a8fca7d278c527b
Storm2601 <7471@holbertonstudents.com>
2023-10-19 23:53:49 -0700

    imprime _putchar, suivi d’une nouvelle ligne et le programme doit retourner 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	_putchar('_');
@@ -10,6 +17,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+
 	_putchar('\n');
 
 	return (0);
