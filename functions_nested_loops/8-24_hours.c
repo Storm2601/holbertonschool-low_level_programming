904162773143379d5700e55bb9e9aa7ac48294e9
Storm2601 <7471@holbertonstudents.com>
2023-10-21 06:42:39 -0700

    imprime l heure toutes les minutes de 00:00 jusqu à 23:59

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,29 +1,27 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_last_digit - Imprime la dernière chiffre d'un nombre
+ * jack_bauer - prints every minute of the day of Jack
+ * Bauer from 00:00 to 23:59
  *
- * void jack_bauer(void);: imprime l'heure de 00:00 à 23:59
- *
- * Return: La valeur du dernier chiffre
  */
 
 void jack_bauer(void)
 {
-	int hour, minute;
-	for (hour = 0; hour < 24; hour++)
-	{
-		for (minute = 0; minute < 60; minute++)
-		{
-			printf("%.2d:%.2d\n", hour, minute);
-			if (hour == 23 && minute == 59)
-			{
-				return;
+	int hour, min;
 
-				jack_bauer();
-				return (0);
-			}
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
 		}
+
 	}
+
 }
