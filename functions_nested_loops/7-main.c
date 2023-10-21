bbd262fef425d90864fe6250d82d2b6d3803df1f
Storm2601 <7471@holbertonstudents.com>
2023-10-21 05:30:31 -0700

    imprime le dernier chiffre d'un nombre et renvoie la valeur du dernie… 9f9af25  · History …r chiffre

--- functions_nested_loops/7-main.c
+++ functions_nested_loops/7-main.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	int r;
 
-	print_last_digit(98);
-	print_last_digit(0);
+	print_last_digit(-1230);
+	print_last_digit(-4558);
 	r = print_last_digit(-1024);
 	_putchar('0' + r);
 	_putchar('\n');
