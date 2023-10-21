cb2a0246cab79676738d8da704db586889c54c6d
Storm2601 <7471@holbertonstudents.com>
2023-10-21 03:05:23 -0700

    calcule la valeur absolue d'un entier

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Calcule la valeur absolue d'un entier
+ *
+ * @n: L'entier dont on veut calculer la valeur absolue
+ *
+ * Return: La valeur absolue de l'entier n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
