b7ea7c54f3e4df7588dcec574ff1fcc73002829c
Storm2601 <7471@holbertonstudents.com>
2023-10-23 01:50:02 -0700

    prend un pointeur sur un int comme paramètre et met à jour la valeur qu'il pointe à 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - prend comme paramètre un pointeur sur un int
+ * met à jour la valeur qu'il pointe à 98.
+ * @n: l'addresse de la variable n
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
