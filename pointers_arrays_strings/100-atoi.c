634dadf9fc71314044cad279dd325add893dd464
Storm2601 <7471@holbertonstudents.com>
2023-10-25 09:45:37 -0700

    yhrz_ygn

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,33 +1,8 @@
 #include "main.h"
 
 /**
- * _atoi - début du programme
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * @s: chaîne de caractères à extraire de
- *
- * Return: renvoie le nombre à partir de la chaîne de caractères
+ * Return: the int converted from the string
  */
-
-int _atoi(char *s)
-{
-	int PosNegDet = 0;
-	int PosNeg;
-	int Digit;
-	int Result = 0;
-
-	for (; (*s < 47 || *s > 58) && (*s != '\0'); s++)
-	{
-		if (*s == 45)
-			PosNegDet++;
-	}
-
-	for (; (*s > 47 && *s < 58) && (*s != '\0'); s++)
-	{
-		Digit = *s - '0';
-		Result = Result * 10 - Digit;
-	}
-
-	PosNeg = (PosNegDet % 2 == 0) ? -1 : 1;
-
-	return (PosNeg * Result);
-}
