de5779dff71e4a3dc62d71cc747a1dc35e59dcf2
Storm2601 <7471@holbertonstudents.com>
2023-10-25 09:49:45 -0700

    fonction qui convertit une chaîne de caractères en un nombre entier

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,8 +1,44 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * _atoi - convertit une chaîne de caractères en un entier.
+ * @s: chaîne de caractères à convertir.
  *
- * Return: the int converted from the string
+ * Return: int converti à partir de la chaîne
  */
+int _atoi(char *s)
+{
+	int d, n, len, f, digit;
+
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	for (len = 0; s[len] != '\0'; len++)
+		;
+
+	for (int i = 0; i < len && f == 0; i++)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+	}
+
+	if (f == 0)
+		return 0;
+
+	return n;
+}
