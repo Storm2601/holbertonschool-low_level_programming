990517202359f118377f9fdc350fce2199ce2154
Storm2601 <7471@holbertonstudents.com>
2023-11-02 06:32:31 -0700

    fonction qui dessine une ligne droite dans le terminal en utilisant que -putchar

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_line - trace une ligne droite dans le terminal
+* @n: nombre de fois que le caractère "_" doit être imprimé
+*/
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1 ; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
