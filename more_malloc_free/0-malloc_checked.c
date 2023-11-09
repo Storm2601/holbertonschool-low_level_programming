3b07c1c67a6f067b2222b45bbc4ea15440fac50b
Storm2601 <7471@holbertonstudents.com>
2023-11-09 07:05:50 -0800

    alloue de la mémoire à l aide de malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - alloue de la mémoire
+* @b: quantité d'octets
+*
+* Return: pointeur sur la mémoire allouée
+* Si malloc échoue, le statut est égal à 98
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
