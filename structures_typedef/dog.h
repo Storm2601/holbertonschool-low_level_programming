1cce637b1082a47e66a65bbb350885febce3b35c
Storm2601 <7471@holbertonstudents.com>
2023-11-14 03:01:52 -0800

    édition dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,7 +7,7 @@
 * @age: Deuxième membre
 * @owner: Troisième membre
 *
-* Description: Description plus longue
+* Description: le nom du chien, son âge et le propriétaire
 */
 struct dog
 {
@@ -26,4 +26,6 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+void free_dog(dog_t *d);
+
 #endif
