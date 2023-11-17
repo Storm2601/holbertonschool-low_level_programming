37e362f0fff163f149c0deb848ae9b43575c4f58
Storm2601 <7471@holbertonstudents.com>
2023-11-17 02:49:44 -0800

    renvoie la somme de tous ses paramètres

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -5,23 +5,23 @@
 /**
 * sum_them_all - renvoie la somme de tous ses paramètres
 *
-* @n : premier paramètre
+* @n: premier parametre
 *
 * Return: somme de tous les paramètres de la fonction
 */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap ;
+	va_list ap;
 	unsigned int i, sum = 0;
 
-	si (n == 0)
-		retour (0);
+	if (n == 0)
+		return (0);
 	va_start(ap, n);
 
-	for (i = 0 ; i < n ; i++)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ap, int);
 	}
 	va_end(ap);
-	return (somme);
+	return (sum);
 }
