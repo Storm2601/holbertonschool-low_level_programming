a378621d1cc36e783147ae786ee626b894aea5f0
Storm2601 <7471@holbertonstudents.com>
2023-11-29 02:40:23 -0800

    imprime tous les éléments d'une liste list_t

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -5,8 +5,9 @@
  * print_list - imprime tous les éléments d'une liste chaînée
  * @h: pointeur sur la liste list_t à imprimer
  *
- * Return: le nombre de nœuds
+ * Return: le nombre de noeuds
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
@@ -29,7 +30,7 @@ size_t print_list(const list_t *h)
  * (indicatif d'une chaîne vide ou nulle).
  * Sinon, passe à l'instruction suivante
  *
- * else printf("[%u] %s\n", h->len, h->str);: Si le champ str n'est pas nul,
+ * else printf("[u] s\n", h->len, h->str);: Si le champ str n'est pas nul,
  * imprime la longueur de la chaîne (h->len)
  * suivie de la chaîne elle-même (h->str)
  *
