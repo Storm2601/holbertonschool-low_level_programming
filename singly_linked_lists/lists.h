63f971b00f11c2c42e70e2286f27c1524d1ff91c
Storm2601 <7471@holbertonstudents.com>
2023-11-29 01:03:57 -0800

    contient les prototypes

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H
+#define LISTS_H
+/**
+* struct list_s - liste chaînée simple
+* @str : string - (chaîne de caractères malloc)
+* @len : longueur de la chaîne
+* @next : pointe vers le noeud suivant
+*
+* Description: structure de nœuds d'une liste simplement chaînée
+*/
+
+typedef struct list_s
+{
+	char *str ;
+	unsigned int len ;
+	struct list_s *next ;
+} list_t ;
+
+size_t print_list(const list_t *h) ;
+size_t list_len(const list_t *h) ;
+list_t *add_node(list_t **head, const char *str) ;
+list_t *add_node_end(list_t **head, const char *str) ;
+
+#endif
