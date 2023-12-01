6acb40c8aa1713e506c6f7a3bd78409efc3d95ec
Storm2601 <7471@holbertonstudents.com>
2023-12-01 19:54:29 +0100

    ajoute un nouveau nœud au début d'une liste de dlistint_t et retourne l'adresse du nouvel élément, ou NULL en cas d'échec

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,37 +1,29 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the beginning
- * of a dlistint_t list
- *
- * @head: head of the list
- * @n: value of the element
- * Return: the address of the new element
- */
+* add_dnodeint - adds a new node at the beginning of the list
+* @head: head of the list
+* @n: data
+* Return: address of the new element or NULL if it fails
+*/
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
-	dlistint_t *h;
 
-	new = malloc(sizeof(dlistint_t));   /** allocation de mémoire pour new */
-		return (NULL);
+	new = malloc(sizeof(dlistint_t));
 
-	new->n = n;     /** affecte la valeur n à n pointé par new */
-	new->prev = NULL;
-	h = *head;  /** initialise nouveau pointer sans modif la valeur de head */
-
-	if (h != NULL)
+	if (new == NULL)
 	{
-		while (h->prev != NULL) /** parcour la liste cha^née en remontant */
-			h = h->prev;    /** depuis 1 élément quelconque vers 1 élément */
+		return (NULL);
 	}
-
-	new->next = h;
-
-	if (h != NULL)
-		h->prev = new;  /** affecte la veleur new a prev pointé par h */
-
-	*head = new;        /** pointer head est égal à new */
-
+	new->prev = NULL;
+	new->n = n;
+	new->next = *head;
+	if (*head != NULL)
+	{
+		(*head)->prev = new;
+	}
+	*head = new;
 	return (new);
 }
