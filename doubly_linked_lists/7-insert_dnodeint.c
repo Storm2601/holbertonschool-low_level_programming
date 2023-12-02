9fed05796555231bb7bb0728c994615b8ae132c5
Storm2601 <7471@holbertonstudents.com>
2023-12-02 16:23:40 +0100

    insère un nouveau nœud à une position donnée, où idx est l'indice de la liste où le nouveau noeud doit être ajouté

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new_node->next = *h;
 		if (*h != NULL)
-		    (*h)->prev = new_node;
+			(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
