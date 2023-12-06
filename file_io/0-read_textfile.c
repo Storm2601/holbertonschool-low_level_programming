8cfc13715121f70bec3c94c6eac3ec4c7d37a452
Storm2601 <kevinguedon2601@gmail.com>
2023-12-06 15:32:26 +0100

    fonction qui lit un fichier texte et l'imprime sur la sortie standard POSIX

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
+
 /**
 * read_textfile - lis un fichier texte et l'imprime sur stdout
 * @filename: nom du fichier
 * @letters: nombre de lettres qu'il doit lire et imprimer
+*
 * Return: nombre réel de lettres qu'il peut lire et imprimer
 */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int o, r, w;
@@ -15,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-buf = malloc(sizeof(char));
+buf = malloc(sizeof(char) * letters);
 if (buf == NULL)
 		return (0);
 
