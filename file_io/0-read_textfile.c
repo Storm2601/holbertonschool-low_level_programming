fb8d989647dc2d554965399b336a4ab580b41cf8
Storm2601 <kevinguedon2601@gmail.com>
2023-12-06 15:23:56 +0100

    fonction qui lit un fichier texte et l'imprime sur la sortie standard POSIX

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,26 +12,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	char *buf;
 
-	buf = malloc(letters * sizeof(char));
-
 	if (filename == NULL)
 		return (0);
 
+buf = malloc(sizeof(char));
+if (buf == NULL)
+		return (0);
+
 	o = open(filename, O_RDWR);
-
-	if (o == -1)
-		return (0);
-
 	r = read(o, buf, letters);
-
-	if (r == -1)
-		return (0);
-
 	w = write(STDOUT_FILENO, buf, r);
 
-	if (w == -1)
-		return (0);
+	if (o == -1 || r == -1 || w == -1)
+{
+	return (0);
+}
 
 	free(buf);
+	close (o);
 	return (w);
 }
