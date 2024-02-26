4d622f549f7f5774c3a0e5a09094be61f42a6f9b
Storm2601 <kevinguedon2601@gmail.com>
2024-02-26 08:59:10 +0000

    add functions for main.h

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+Prototype: int _isupper(int c);
+Prototype: int _isdigit(int c);
+Prototype: int mul(int a, int b);
+Prototype: void print_numbers(void);
+Prototype: void print_most_numbers(void);
+Prototype: void more_numbers(void);
+Prototype: void print_line(int n);
+Prototype: void print_diagonal(int n);
+Prototype: void print_square(int size);
+Prototype: void print_triangle(int size);
+
+#endif
